86d1d79eee6ab9c00fba9e14f65dc2c7c77cdc7a
Vardhak Kore <vardhakkore@gmail.com>
2023-08-01 11:15:27 +0530

    files added to repo

---
+++ public/js/navbar.js
@@ -0,0 +1,8 @@
+function showMenu(name) {
+    var menu = document.querySelector(name);
+    menu.style.display = "flex";
+}
+function closeMenu(name) {
+    var menu = document.querySelector(name);
+    menu.style.display = "none";
+}
---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const fs = require("fs");
+const hbs = require("hbs");
+const path = require("path");
+
+const app = express();
+
+const port = 80;
+const hostname = "0.0.0.0";
+
+app.use(express.static("public"));
+
+// const partials_path = path.join(__dirname, "/partials");
+const partials_path = "public/partials"
+
+app.set("view engine", "hbs");
+
+hbs.registerPartials(partials_path);
+
+
+const jsonData = JSON.parse(fs.readFileSync("public/data/data.json", "utf-8"));
+
+
+app.get("/", (req, res) => {
+    res.render("home", {
+        title: "CodeHub community",
+        services_cards: jsonData,
+    });
+})
+
+app.get("/about.hbs", (req, res) => {
+    res.render("about");
+})
+
+app.get("*", (req, res) => {
+    res.render("404");
+})
+
+app.listen(port, hostname, () => {
+    console.log(`listening to port ${port}....`);
+})
